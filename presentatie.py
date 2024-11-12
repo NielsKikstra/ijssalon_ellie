ff82b12a22d64bf1f3c11deca81d4942cb8283b9
Niels Kikstra <nkzzow@gmail.com>
2024-11-13 00:48:16 +0100

    Les 10 huiswerk toegevoegd

---
+++ presentatie.py
@@ -0,0 +1,10 @@
+def presenteer(mijn_dict, totaal):
+    for k, v in mijn_dict.items():
+        print(f"{k} : {v} euro")
+    
+    print(26 * "=")
+    
+    print(f"totaal : {totaal} euro")
+
+
+
